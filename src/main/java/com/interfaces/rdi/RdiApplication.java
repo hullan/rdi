084d5aeec8c95b29f4a411bc479394281a92b736
hullan <inazio@gmail.com>
2022-10-30 08:48:41 +0100

    inital commit

---
+++ src/main/java/com/interfaces/rdi/RdiApplication.java
@@ -0,0 +1,13 @@
+package com.interfaces.rdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interfaces/rdi/RdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interfaces.rdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
